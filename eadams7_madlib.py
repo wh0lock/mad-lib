bc8400d22d310b58dfb5c78ea0bd09e2aa1495b7
wh0lock <165333619+wh0lock@users.noreply.github.com>
2025-10-15 11:18:38 -0400

    Add files via upload

---
+++ eadams7_madlib.py
@@ -0,0 +1,31 @@
+# Emily Adams
+# Mad Lib
+# Started August 18, 2025, due August 22, 2025
+# This project is a simple mad lib based on a portion of "If You Give a Mouse a Cookie" by Laura Joffe Numeroff.
+
+# variables
+animal = input("Please give me the name of an animal: ")
+food = input("Please give me the name of a food item: ")
+drink = input("Please give me the name of a drink: ")
+utensil = input("Please give me the name of a utensil: ")
+item = input("Please give me the name of any random item: ")
+verb = input("Please give me an example of a verb: ")
+place = input("Please give me an example of a place: ")
+activity = input("Please give me an example of an activity: ")
+characteristic = input("Please give me a random characteristic: ")
+tool = input("Please list any random tool: ")
+adverb = input("Please give me an example of an adverb: ")
+tool2 = input("Please list another random tool: ")
+adverb2 = input("Please give me another example of an adverb: ")
+verb2 = input("Please give another example of verb: ")
+item2 = input("Please give me the name of another random item: ")
+place2 = input("Please give me another example of a place: ")
+
+# story
+print(f"If you give a {animal} a {food}, he's going to ask for a {drink}.")
+print(f"When you give him the {drink}, he'll probably ask you for a {utensil}.")
+print(f"When he's finished, he'll ask for a {item}.")
+print(f"Then he'll want to {verb} in a {place} to make sure he doesn't {activity}.")
+print(f"When he {verb} into the {place}, he might notice that he looks {characteristic}, so he'll probably ask for a {tool}.")
+print(f"When he's finished {adverb}, he'll want a {tool2}. He'll start {adverb2}.")
+print(f"He might get carried away and {verb2} every {item2} in the {place2}.")
